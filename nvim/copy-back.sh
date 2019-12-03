657e913599a4c906c69d7bd14c3387a8a57ea565
lihu <lihu@eventslack.com>
2019-12-03 13:04:51 +0800

    update copy-back .sh

--- nvim/copy-back.sh
+++ nvim/copy-back.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 CURRENT_DIR=`dirname "${BASH_SOURCE-$0}"`
 CURRENT_DIR_HOME=`cd "$CURRENT_DIR">/dev/null;cd .; pwd`
-if [ ! -d "~/.config/nvim/" ];then
+if [ ! -d ~/.config/nvim ];then
+    echo "Target dir not exist"
     mkdir ~/.config/nvim
 fi
 
