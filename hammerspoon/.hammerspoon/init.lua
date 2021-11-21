e5b56499a362cb14c903c2bb51a89b3cdfafc9ce
SilentStorm <1449488533qq@gmail.com>
2021-11-21 23:28:06 +0800

    feat: add repeating keys shortcut for window movement

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -1,59 +1,65 @@
 --hammerspoon
 --向左移动窗口位置
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "H", function()
+
+local function left_move_window()
   local win = hs.window.focusedWindow()
   local f = win:frame()
   f.x = f.x - 20
   win:setFrame(f)
-end)
+end
 
---向右移动窗口位置
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "L", function()
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "H", left_move_window,nil,left_move_window)
+
+local function right_move_window()
   local win = hs.window.focusedWindow()
   local f = win:frame()
   f.x = f.x + 20
   win:setFrame(f)
-end)
+end
 
+--向右移动窗口位置
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "L", right_move_window,nil,right_move_window)
 
---向下移动窗口位置
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "J", function()
+local function down_move_window()
   local win = hs.window.focusedWindow()
   local f = win:frame()
   f.y = f.y + 20
   win:setFrame(f)
-end)
+end
 
---向上移动窗口位置
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "K", function()
+--向下移动窗口位置
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "J",down_move_window,nil, down_move_window)
+
+local function up_move_window()
   local win = hs.window.focusedWindow()
   local f = win:frame()
   f.y = f.y - 20
   win:setFrame(f)
-end)
+end
+
+--向上移动窗口位置
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "K",up_move_window,nil,up_move_window)
 
 
 --最小化所有的窗口
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "M", function()
   local allWindows = hs.window.allWindows()
-  for k,win in ipairs(allWindows) do
+  for _,win in ipairs(allWindows) do
       if not win:isMinimized() then
           win:minimize()
       end
   end
-  
 end)
 
 
 --取消最小化的窗口
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "U", function()
   local allWindows = hs.window.allWindows()
-  for k,win in ipairs(allWindows) do
+  for _,win in ipairs(allWindows) do
       if win:isMinimized() then
           win:unminimize()
       end
   end
-  
 end)
 
 hs.loadSpoon("ModalMgr")
