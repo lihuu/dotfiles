5cc6ce9df7a37aa3109cd04bb12f72b6bacaa214
SilentStorm <1449488533qq@gmail.com>
2023-07-11 00:21:18 +0800

    feat: add k8s install script

---
+++ scripts/kubernetes/install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+sudo apt install -y apt-transport-https ca-certificates curl
+
+curl https://mirrors.aliyun.com/kubernetes/apt/doc/apt-key.gpg | sudo apt-key add -
+
+cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
+deb https://mirrors.aliyun.com/kubernetes/apt/ kubernetes-xenial main
+EOF
+
+sudo apt update
