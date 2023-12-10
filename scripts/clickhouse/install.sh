2ec1d208e6121e6bca0993e1b1e176aaa919ac6e
SilentStorm <1449488533qq@gmail.com>
2023-12-10 20:26:40 +0800

    feat: fix clickhouse install script

--- scripts/clickhouse/install.sh
+++ scripts/clickhouse/install.sh
@@ -9,3 +9,6 @@ sudo chmod +r /usr/share/keyrings/clickhouse-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/clickhouse-keyring.gpg] https://packages.clickhouse.com/deb stable main" | sudo tee \
 	/etc/apt/sources.list.d/clickhouse.list
 sudo apt-get update
+
+sudo apt-get install -y clickhouse-server clickhouse-client clickhouse-keeper
+
