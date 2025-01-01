2b7beb2a5caa099558b54f1b5b37248ddc6440bf
Hu Li <lihu@Hus-MacBook-Pro.local>
2025-01-01 16:52:37 +0800

    feat: add software install script

---
+++ homebrew/brew-install-software.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+brew install gh
+brew install alacritty --cask
+brew install hammerspoon --cask
+brew install dbeaver-community --cask
+brew install iterm2 --cask
+brew install drawio --cask
+brew install jd-gui --cask
+brew install finalshell --cask
+brew install kitty --cask
+brew install font-blex-mono-nerd-font --cask
+brew install Lepton --cask
+brew install font-fira-mono-nerd-font --cask
+brew install macvim --cask
+brew install font-hack-nerd-font --cask
+brew install mysqlworkbench --cask
+brew install font-jetbrains-mono --cask
+brew install neovide --cask
+brew install font-jetbrains-mono-nerd-font --cask
+brew install picgo --cask
