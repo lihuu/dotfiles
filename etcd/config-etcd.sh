9943209640fb59a2c3c95d5a90c993f9038fbda8
SilentStorm <1449488533qq@gmail.com>
2025-05-22 23:12:43 +0800

    fix: config-etcd.sh

--- etcd/config-etcd.sh
+++ etcd/config-etcd.sh
@@ -7,11 +7,11 @@ sudo mkdir -p /etc/kubernetes/pki
 sudo mkdir -p /etc/etcd/pki
 sudo mkdir -p /etc/etcd/data
 sudo cp $SCRIPT_DIR/ca.* /etc/kubernetes/pki
-sudo cp $SCRIPT_DIR/etcd_server.* /etc/etcd/pki
-sudo cp $SCRIPT_DIR/etcd_client.* /etc/etcd/pki
+sudo cp $SCRIPT_DIR/$(hostname)/etcd_server.* /etc/etcd/pki
+sudo cp $SCRIPT_DIR/$(hostname)/etcd_client.* /etc/etcd/pki
 sudo chown -R etcd /etc/etcd/data
 sudo chown -R etcd /etc/etcd/pki
 sudo chmod +r /etc/etcd/pki/*
 sudo mv /etc/default/etcd /etc/default/etcd.bak
-sudo cp $SCRIPT_DIR/$(hostname).etcd.conf /etc/etcd/etcd.conf
+sudo cp $SCRIPT_DIR/$(hostname)/etcd.conf /etc/etcd/etcd.conf
 sudo ln -s /etc/etcd/etcd.conf /etc/default/etcd
