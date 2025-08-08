62c26dea8db17ece041089cf27e4be7c7333a2f7
SilentStorm <1449488533qq@gmail.com>
2025-08-08 10:16:03 +0800

    feat: update brew install software scripts

---
+++ homebrew/linux/brew-install-software.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+brew install gh
+brew install bat
+brew install hugo
+brew install ranger
+brew install font-blex-mono-nerd-font --cask
+brew install font-fira-mono-nerd-font --cask
+brew install font-hack-nerd-font --cask
+brew install font-jetbrains-mono --cask
+brew install neovim
+brew install neovide
+brew install font-jetbrains-mono-nerd-font --cask
+
+brew tap daipeihust/tap
+brew install im-select
